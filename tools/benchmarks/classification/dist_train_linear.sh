61dae54ed469d484230408452a2b568255ed7be1
Dung <dinhanhdung1996@gmail.com>
2021-12-17 08:57:18 +0700

    save change

--- tools/benchmarks/classification/dist_train_linear.sh
+++ tools/benchmarks/classification/dist_train_linear.sh
@@ -5,8 +5,9 @@ set -x
 
 CFG=$1  # use cfgs under "configs/benchmarks/classification/imagenet/*.py"
 PRETRAIN=$2  # pretrained model
-PY_ARGS=${@:3}
-GPUS=${GPUS:-8}  # When changing GPUS, please also change imgs_per_gpu in the config file accordingly to ensure the total batch size is 256.
+GPUS=$3
+PY_ARGS=${@:4}
+# GPUS=${GPUS:-8}  # When changing GPUS, please also change imgs_per_gpu in the config file accordingly to ensure the total batch size is 256.
 PORT=${PORT:-29500}
 
 # set work_dir according to config path and pretrained model to distinguish different models
