46064508fff433fa709a7bb3e9f962f281b1a6b9
Dinh Anh Dung <dinhanhdung1996@gmail.com>
2021-12-30 07:21:29 +0800

    save change

--- tools/dist_train_cluster.sh
+++ tools/dist_train_cluster.sh
@@ -5,9 +5,9 @@ CONFIG=$2
 GPUS=$3
 PORT=${PORT:-29501}
 PYTHON=${PYTHON:-"/home/users/${USER}/.conda/envs/openss/bin/python3.8"}
-WORK_DIR="$(echo ${CFG%.*} | sed -e "s/configs/work_dirs/g")/$(echo $PRETRAIN | rev | cut -d/ -f 1 | rev)"
+WORK_DIR="$(echo ${CONFIG%.*} | sed -e "s/configs/work_dirs/g")/"
 AWORK_DIR="../../scratch/dso/openss/${WORK_DIR}"
 
 PYTHONPATH="$(dirname $0)/..":$PYTHONPATH \
 $PYTHON -m torch.distributed.launch --nproc_per_node=$GPUS --master_port=$PORT \
-    $(dirname "$0")/train.py $CONFIG --work_dir $AWORK_DIR --seed 0 --launcher pytorch ${@:3}
+    $(dirname "$0")/train.py $CONFIG --work_dir $AWORK_DIR --seed 0 --launcher pytorch ${@:4}
