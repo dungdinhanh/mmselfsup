0052c43d3086f0d549dbaa3bdb6c4a7c68d85106
thibaongoc <thibaongoc_nguyen@sutd.edu.sg>
2022-03-06 18:21:55 +0800

    save change

--- mmselfsup/models/algorithms/__init__.py
+++ mmselfsup/models/algorithms/__init__.py
@@ -17,6 +17,7 @@ from .swav import SwAV
 from .simsiam_kd import SimSiamKD, SimSiamKDZT, SimSiamKD_PredMatching, SimSiamKD_GT, SimSiamKD_wNeg,\
     SimDis_Siam_simplified, SimSiamKD_PoswNeg, SimDis_PoswNeg, SimDis_Pos, SimDis_wNeg
 from .simsiam_kd_test import *
+from .simdis_kd_test import *
 __all__ = [
     'BaseModel', 'BYOL', 'Classification', 'DeepCluster',
     'DenseCL', 'MoCo', 'NPID', 'ODC', 'RelativeLoc',
